ae748b902a6455422ca380b977e6e5d50b108f82
harry <harmanmanchanda182@gmail.com>
2017-10-14 01:49:46 +0530

    Interceptors!

--- src/main.js
+++ src/main.js
@@ -3,7 +3,19 @@ import VueResource from 'vue-resource';
 import App from './App.vue';
 
 Vue.use(VueResource);
-Vue.http.options.root = 'https://vuejs-http-6a850.firebaseio.com/data.json';
+Vue.http.options.root = 'https://vuejs-http-interceptors.firebaseio.com/data.json';
+Vue.http.interceptors.push((request, next) => {
+  console.log(request);
+  if (request.method == 'POST')
+    request.method = 'PUT';
+  next(response => {
+    response.json = () => {
+      return { 
+        messages: response.body 
+      };
+    };
+  });
+});
 
 new Vue({
   el: '#app',
